ba21af3916984b1b17e1daae54228e4d0374a14f
Low Yiyiu <10480968+lowyiyiu@users.noreply.github.com>
2025-01-16 23:36:39 +0800

    Updated product slider for Home page

--- app/components/ProductSlider.js
+++ app/components/ProductSlider.js
@@ -6,24 +6,11 @@ import { gsap } from "gsap";
 import Image from "next/image";
 import { productSliderScenes } from "@/data/productSliderData";
 import WaveAnimation from "./waveAnimation";
-import { FiArrowRightCircle } from "react-icons/fi";
-import { FiArrowLeftCircle } from "react-icons/fi";
-
-import useAnchorPosition from "../hooks/useAnchorPosition";
 
 export default function ProductSlider() {
-  //to do anchoring
-  const productImageRef = useRef(null); // Ref for the product image to be used as the anchor
-
-  const anchorPosition = useAnchorPosition(productImageRef); // Collect anchor positions
-
   //to do sliding
 
   const [currentScene, setCurrentScene] = useState(0); // Track active scene
-
-  const [indicatorStart, setIndicatorStart] = useState(0);
-
-  const [indicatorsToShow, setIndicatorsToShow] = useState(3);
 
   //to track is animating or not, so that we don't play animation while animating
 
@@ -31,62 +18,34 @@ export default function ProductSlider() {
 
   const sceneRefs = useRef([]); // initiate an empty array to hold references to the total scene divs
 
-  const totalScenes = productSliderScenes.length;
-
-  // Calculate transform style for sliding effect based on `indicatorStart`
-  // Calculate transform style for sliding effect based on `indicatorStart`
-  const transformStyle = {
-    transform: `translateX(-${indicatorStart * 60}px)`,
-    transition: "transform 0.5s ease-in-out",
-  };
   // Function to handle the previous set of indicators
 
   //to handle next scene and previous scene
 
   // Render a function that tracks the next scene and the previous scene, based on the state of the currently tracked scene
 
-  const nextScene = () => {
+  const goToScene = (index) => {
+    if (!isAnimating) {
+      renderScene(index);
+    }
+  };
+
+  const goToNextScene = () => {
     if (!isAnimating) {
       const next = (currentScene + 1) % productSliderScenes.length;
-      playSceneAnimation(next, 1);
-
-      // Wrap `indicatorStart` for infinite loop effect
-      if (indicatorStart + 1 >= totalScenes) {
-        setIndicatorStart(0); // Reset to start after reaching the end
-      } else {
-        setIndicatorStart(indicatorStart + 1);
-      }
+      renderScene(next);
     }
   };
 
-  const prevScene = () => {
+  const goToPrevScene = () => {
     if (!isAnimating) {
       const prev = (currentScene - 1 + productSliderScenes.length) % productSliderScenes.length;
-      playSceneAnimation(prev, -1);
-
-      // Wrap `indicatorStart` for infinite loop effect
-      if (indicatorStart - 1 < 0) {
-        setIndicatorStart(totalScenes - 1); // Wrap to the end if going before the first
-      } else {
-        setIndicatorStart(indicatorStart - 1);
-      }
-    }
-  };
-
-  // render a function that plays the scene animation
-  const goToScene = (index) => {
-    if (!isAnimating && index !== currentScene) {
-      const direction = index > currentScene ? 1 : -1; // Determine animation direction
-      playSceneAnimation(index, direction);
-    }
-
-    if (index >= indicatorStart + indicatorsToShow || index < indicatorStart) {
-      setIndicatorStart(index);
+      renderScene(prev);
     }
   };
 
   // Function to play the timeline animation for each scene
-  const playSceneAnimation = (nextIndex, direction) => {
+  const renderScene = (nextIndex) => {
     const currentElement = sceneRefs.current[currentScene];
     const nextElement = sceneRefs.current[nextIndex];
 
@@ -101,7 +60,7 @@ export default function ProductSlider() {
       });
       // Initial setup - only set opacity for container, not children
       // i set my nextElement, which is based on the nextIndex state that is passed into the playScene
-      // the argument is passed in when i click the button, it runs the function prevScene or nextScene, which runs playScene
+      // the argument is passed in when i click the button, it runs the function goToPrevScene or goToNextScene, which runs playScene
 
       //I first set next element to be invisible since by default it is not
       gsap.set(nextElement, {
@@ -252,119 +211,85 @@ export default function ProductSlider() {
   // Initialize the first scene (make sure it's visible) and create context for animations
 
   useEffect(() => {
-    console.log(`useEffect fired, current scene is ${currentScene}`);
     const ctx = gsap.context(() => {
       gsap.set(sceneRefs.current[currentScene], { opacity: 1 });
     });
 
+    let resizeTimer;
+
+    window.onresize = () => {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    };
+
     return () => ctx.revert(); // Clean up on component unmount
   }, []);
 
-  useEffect(() => {
-    const updateIndicatorsToShow = () => {
-      setIndicatorsToShow(window.innerWidth >= 1024 ? 5 : 3); // 5 on desktop (1024px and up), 3 on mobile/tablet
-    };
-
-    updateIndicatorsToShow(); // Initial check
-    window.addEventListener("resize", updateIndicatorsToShow); // Listen for screen resize
-
-    return () => window.removeEventListener("resize", updateIndicatorsToShow); // Clean up on component unmount
-  }, []);
-
   return (
-    <div className="relative h-screen w-screen">
+    <div className="relative">
       {/* Slider Container */}
-      <div className="relative h-full w-screen overflow-hidden">
-        <Image
-          alt="stage-ring"
-          className="stage-ring absolute left-[50%] top-[53%] z-[16] translate-x-[-50%] translate-y-[-80%] lg:top-[55%] xl:top-[58%] 2xl:top-[55%]"
-          width={800}
-          height={800}
-          src={"/images/stage-ring.png"}
-          style={{
-            width: anchorPosition.width * 1.05,
-          }}
-        />
-        <Image alt="stage-shadow" className="stage-shadow absolute bottom-0 z-[18] h-[48%] w-full object-cover object-top sm:h-[45%] 2xl:h-[48%]" width={2000} height={1000} src={"/images/stage-shadow.png"} />
+      <div className="relative w-screen overflow-hidden pb-[60%]">
+        <Image alt="stage-ring" className="stage-ring absolute left-0 top-0 z-[16] w-1/3 translate-x-full translate-y-[60%]" width={1745} height={1074} src={"/images/stage-ring.png"} />
+        <Image alt="stage-shadow" className="stage-shadow absolute top-0 z-[18] w-full translate-y-3/4" width={2814} height={1018} src={"/images/stage-shadow.png"} />
 
         {productSliderScenes.map((scene, index) => {
           return (
             <div
               key={scene.id}
               ref={(el) => (sceneRefs.current[index] = el)} // on first render, I map through the scenes and add it to the sceneRefs array, which is a ref
-              className="absolute top-0 flex h-full w-full justify-center opacity-0" // Initially hidden
+              className="absolute h-screen w-screen opacity-0" // Initially hidden
             >
               {/* Product Banner */}
-              <Image
-                className="product-banner absolute left-[8%] top-[50%] z-20 overflow-hidden md:left-[8%] md:block lg:left-[16%] xl:left-[25%] 2xl:left-[31%]"
-                width={300}
-                height={300}
-                alt="product-banner"
-                src={"/images/product-animations/activated-ginko/activated-ginkgo-banner.png"}
-                style={{
-                  width: anchorPosition.width * 0.5,
-                }}
-              />
+              <Image className="product-banner absolute left-0 top-0 z-20 w-1/4 translate-x-full translate-y-[220%] scale-75" width={948} height={364} alt="product-banner" src={"/images/product-animations/activated-ginko/activated-ginkgo-banner.png"} />
 
               {/* Circular */}
-
-              <Image
-                className="product-circular absolute right-[25%] top-[30%] z-20 overflow-hidden sm:right-[30%] sm:top-[28%] md:right-[27%] md:top-[25%] md:block lg:right-[30%] 2xl:right-[37%]"
-                src={scene.images.circular}
-                alt="circular"
-                width={200}
-                height={200}
-                style={{
-                  width: anchorPosition.width * 0.3,
-                }}
-              />
+              <Image className="product-circular absolute right-0 top-0 z-[24] w-1/6 -translate-x-[180%] translate-y-1/2 scale-75" src={scene.images.circular} alt="circular" width={601} height={588} />
 
               {/* Product Image */}
-              <Image ref={productImageRef} src={scene.images.product} alt="product" width={800} height={800} className="product-image z-20 w-[60vw] object-contain lg:w-[550px]" />
+              <Image src={scene.images.product} alt="product" width={1000} height={1000} className="product-image absolute top-0 z-[22] w-1/3 translate-x-full translate-y-1/2 scale-90" />
 
               {/* Background Image */}
-              <Image width={2000} height={800} alt="background" src={scene.images.bg} className="bg-image absolute h-full w-full object-cover" />
+              <Image width={2691} height={2485} alt="background" src={scene.images.bg} className="bg-image absolute z-[14] w-full object-cover" />
+
               {/* Human Image */}
-              <Image
-                src={scene.images.human}
-                alt="human"
-                width={500}
-                height={600}
-                className="human-image absolute right-[-15%] top-[45%] z-20 sm:right-[-10%] sm:top-[33%] md:right-[-20%] lg:right-[-10%] xl:right-[-5%] 2xl:right-[2%]"
-                style={{
-                  width: anchorPosition.width,
-                  // top: anchorPosition.top,
-                  // // left: anchorPosition.left - 0.5 * anchorPosition.width,
-                  // left: anchorPosition.left,
-                }}
-              />
+              <Image src={scene.images.human} alt="human" width={1813} height={1675} className="human-image absolute right-0 top-0 z-[24] w-1/3 -translate-x-[45%] translate-y-1/2 scale-75" />
 
               {/* Text Container */}
-              <div className="text-container absolute left-[5%] top-[10%] z-20 w-[30%]">
+              <div className="text-container absolute left-0 top-0 z-20 translate-x-[5%] translate-y-[30%] lg:translate-y-[40%] xl:translate-y-[70%] 2xl:translate-y-[90%]">
                 <h1 className="mb-8 text-2xl text-tsdarkgreen xl:text-4xl">{scene.title}</h1>
-                <h3 className="text-sm text-tsdarkgreen md:text-xl">{scene.description}</h3>
+                <h3 className="max-w-[22%] text-xs text-tsdarkgreen md:text-sm lg:text-xl">{scene.description}</h3>
               </div>
             </div>
           ); // Anchor to the product image
         })}
-      </div>
 
-      <div className="absolute bottom-[10%] left-1/2 z-30 flex h-auto w-3/4 -translate-x-1/2 flex-row items-center justify-between 2xl:w-1/2">
-        <button onClick={prevScene} className="z-[31] text-tsdarkgreen">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-          </svg>
-        </button>
-        {productSliderScenes.map((scene, index) => (
-          <div key={index}>
-            <Image src={scene.images.featured} alt={scene.title} width={192} height={192} className={`mx-auto transform rounded-full object-cover transition-all duration-150 ease-in-out ${currentScene === index ? "scale-150" : "opacity-85 blur-[1px] brightness-90"}`} />
+        <div className="absolute top-0 z-50 w-full translate-y-[800%] lg:translate-y-[750%]">
+          <div className="mx-auto flex h-auto w-3/5 flex-row items-center justify-between">
+            <button onClick={goToPrevScene} className="z-[31] text-tsdarkgreen">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 md:size-10 lg:size-12">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+              </svg>
+            </button>
+            {productSliderScenes.map((scene, index) => (
+              <div
+                onClick={() => {
+                  goToScene(index);
+                }}
+                key={index}
+                className="cursor-pointer"
+              >
+                <Image src={scene.images.featured} alt={scene.title} width={192} height={192} className={`mx-auto transform rounded-full object-cover transition-all duration-150 ease-in-out ${currentScene === index ? "scale-150" : "opacity-85 blur-[1px] brightness-90"}`} />
+              </div>
+            ))}
+            <button onClick={goToNextScene} className="z-[31] text-tsdarkgreen">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 md:size-10 lg:size-12">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+              </svg>
+            </button>
           </div>
-        ))}
-        <button onClick={nextScene} className="z-[31] text-tsdarkgreen">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-          </svg>
-        </button>
+        </div>
       </div>
 
       <WaveAnimation />
