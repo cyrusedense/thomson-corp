fe8efc979defd41926a3bbbd2fbc68a84d4f7bfa
Low Yiyiu <10480968+lowyiyiu@users.noreply.github.com>
2025-01-18 16:23:53 +0800

    Update jobListings.js

--- data/jobListings.js
+++ data/jobListings.js
@@ -2,27 +2,13 @@ export const jobs = {
   "en-my": [
     {
       id: 1,
-      title: "Sales Executive",
-      description:
-        "MY JOB-1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sed, ipsum itaque praesentium, provident officia eligendi cupiditate voluptate consequuntur numquam quasi neque cum obcaecati autem soluta. Ut eveniet doloribus repellat.",
-    },
-    {
-      id: 2,
-      title: "Roadshow Promoter",
-      description:
-        "MY JOB-2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sed, ipsum itaque praesentium, provident officia eligendi cupiditate voluptate consequuntur numquam quasi neque cum obcaecati autem soluta. Ut eveniet doloribus repellat.",
-    },
-    {
-      id: 3,
-      title: "Product Manager",
-      description:
-        " MY JOB-3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sed, ipsum itaque praesentium, provident officia eligendi cupiditate voluptate consequuntur numquam quasi neque cum obcaecati autem soluta. Ut eveniet doloribus repellat.",
-    },
-    {
-      id: 4,
-      title: "Data Specialist",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sed, ipsum itaque praesentium, provident officia eligendi cupiditate voluptate consequuntur numquam quasi neque cum obcaecati autem soluta. Ut eveniet doloribus repellat.",
+      title: "Event Mobile Product Advisors",
+      description: "We are looking for individuals to represent our brand of quality supplements, in a way that gives the public confidence, trust and respect, and also to turn them into loyal customers. This position will involve customer facing opportunities in a retail setting. If sharing knowledge to others and achieving goals excites you, come join us!",
+      highlights: [],
+      jobDesc: ["Training is operating a machine for the purpose of events/sales.", "To conduct events at assigned retail location and achieve monthly sales target set by the company", "Promote company's products and conduct product demonstrations to potential customers.", "Setup and pack up for roadshows and events as and when required.", "Ensure products and displays are well presented with proper and correct signages.", "Perform daily and monthly sales report and handle stocks.", "Provide sales feedback and suggestions to your supervisor.", "To carry out any other duties as assigned by area sales manager/ senior event manager.", "Project positive image of the company and the brand."],
+      jobReq: ["Candidates must have a car to carry event materials and able to travel outstation.", "Experience in sales to customers, organize events or experience selling health supplements preferred.", "Candidates must possess at least Secondary Education. Business or Health Science would be added advantage.", "Required to work retail hours.", "Candidates have to be proactive/ self-disciplined/ independent/ responsible/ results driven and possess a strong team spirit with good interpersonal skills.", "Required language (s): English, Bahasa Malaysia and others dialect an asset."],
+      other: ["Retail hours and multi location", "Dress code: company uniform"],
+      email: "",
     },
   ],
 
@@ -30,37 +16,15 @@ export const jobs = {
     {
       id: 1,
       title: "Sales Executive",
-      description:
-        "We are seeking passionate and driven individuals to join our team as Roadshow Sales Promoters in the health supplement industry. Join us in providing high-quality health supplement products that aim to empower individuals to live their healthiest and best lives.",
+      description: "We are seeking passionate and driven individuals to join our team as Roadshow Sales Promoters in the health supplement industry. Join us in providing high-quality health supplement products that aim to empower individuals to live their healthiest and best lives.",
 
-      highlights: [
-        "Positive working environment",
-        "Attractive commission",
-        "Medical benefits",
-        "Training provided",
-      ],
+      highlights: ["Positive working environment", "Attractive commission", "Medical benefits", "Training provided"],
 
-      jobDesc: [
-        "Promote company supplements at retail pharmacies or outlets",
-        "Conduct health events at assigned retail locations, including setting up and dismantling of roadshows if any",
-        "Project positive image of the company and the brand",
-        "Carry out any other duties as and when assigned by Sales Executive/Manage",
-      ],
+      jobDesc: ["Promote company supplements at retail pharmacies or outlets", "Conduct health events at assigned retail locations, including setting up and dismantling of roadshows if any", "Project positive image of the company and the brand", "Carry out any other duties as and when assigned by Sales Executive/Manage"],
 
-      jobReq: [
-        "Min 2 years working experience in Sales",
-        "Proactive/self-disciplined/keen to learn and possess a strong team spirit",
-        "Proficient in Mandarin and English. Dialects and other languages are beneficial",
-        "Work during retail hours (may be PH or weekends)",
-        "Interest in health-related topics are ideal",
-      ],
+      jobReq: ["Min 2 years working experience in Sales", "Proactive/self-disciplined/keen to learn and possess a strong team spirit", "Proficient in Mandarin and English. Dialects and other languages are beneficial", "Work during retail hours (may be PH or weekends)", "Interest in health-related topics are ideal"],
 
-      other: [
-        "Basic with high commission",
-        "Retail hours and multi-location on rotational basis",
-        "7-hour workday, 6-day work week",
-        "Flexible work hours for part time positions available",
-      ],
+      other: ["Basic with high commission", "Retail hours and multi-location on rotational basis", "7-hour workday, 6-day work week", "Flexible work hours for part time positions available"],
 
       email: "enquiry.sg@thomsonhealth.com",
     },
